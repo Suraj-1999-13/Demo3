6ef58e098fd434e86893b7d3252bac53b1a35b1b
Suraj <82825499+Suraj-1999-13@users.noreply.github.com>
2022-03-13 23:53:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,51 @@
-from streamlit_webrtc import webrtc_streamer, RTCConfiguration
+import streamlit as st
+from streamlit_webrtc import webrtc_streamer
 import av
 import cv2
+import mediapipe as mp
+import numpy as np
 
-cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+st.title("My first Streamlit app")
+
+
 
 class VideoProcessor:
-	def recv(self, frame):
-		frm = frame.to_ndarray(format="bgr24")
+    def __init__(self):
+      self.mpHands=mp.solutions.hands
+      self.hands=self.mpHands.Hands(max_num_hands=1)
+      self.mpDraw=mp.solutions.drawing_utils
 
-		faces = cascade.detectMultiScale(cv2.cvtColor(frm, cv2.COLOR_BGR2GRAY), 1.1, 3)
+    def recv(self, frame):
+        img = frame.to_ndarray(format="bgr24")
+        imgRGB=cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        result=self.hands.process(imgRGB)
+        padd_amount = 15
+        if result.multi_hand_landmarks:
+            landmarks = [] 
+            for handLms in result.multi_hand_landmarks:
+                
+                for id, landmark in enumerate(handLms.landmark):
+                    height, width, _ = img.shape
+                    landmarks.append((int(landmark.x * width), int(landmark.y * height),
+                                         (landmark.z * width)))
+                self.mpDraw.draw_landmarks(img, handLms,self.mpHands.HAND_CONNECTIONS)
+                x_coordinates = np.array(landmarks)[:,0]
+                y_coordinates = np.array(landmarks)[:,1]
+                x1  = int(np.min(x_coordinates) - padd_amount)
+                y1  = int(np.min(y_coordinates) - padd_amount)
+                x2  = int(np.max(x_coordinates) + padd_amount)
+                y2  = int(np.max(y_coordinates) + padd_amount)
+                cv2.rectangle(img, (x1, y1), (x2, y2), (155, 0, 255), 3, cv2.LINE_8)
+        return av.VideoFrame.from_ndarray(img, format="bgr24")
 
-		for x,y,w,h in faces:
-			cv2.rectangle(frm, (x,y), (x+w, y+h), (0,255,0), 3)
 
-		return av.VideoFrame.from_ndarray(frm, format='bgr24')
 
-webrtc_streamer(key="key", video_processor_factory=VideoProcessor,
-				rtc_configuration=RTCConfiguration(
-					{"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
-					)
-	)
+
+
+ctx = webrtc_streamer(
+    key="example",
+    video_processor_factory=VideoProcessor,
+    rtc_configuration={  # Add this line
+        "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
+    }
+)
